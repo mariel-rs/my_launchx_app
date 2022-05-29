1b0e74da0761fc21665882dbcd54cc2ac544d4a7
mariel-rs <mariel.rsal@gmail.com>
2022-05-29 00:02:40 -0500

    Create base js project - Create small class and module - Add jest dependency - Modify package.json to start with index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require("./app/missionCommander")
+
+const alicia = new MissionCommander("Alicia")
+const camila = new MissionCommander("Camila")
+const ali = new MissionCommander("Ali")
+
+console.log(ali.name)
